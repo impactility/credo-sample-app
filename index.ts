8068b83f17978e05102818374a08f2974fc3caa0
ShikharBhatt <shikhar.bhatt@shell.com>
2024-08-13 16:17:53 +0530

    feat: call did functions

--- index.ts
+++ index.ts
@@ -1,40 +1,44 @@
-import { DidCreateOptions, InitConfig, Key, KeyDidCreateOptions, KeyType, TypedArrayEncoder, WalletApi, WalletConfig, WalletCreateKeyOptions, Ed25519Jwk } from "@credo-ts/core";
-import { Agent, Wallet } from '@credo-ts/core'
+import { InitConfig } from "@credo-ts/core";
+import { Agent } from '@credo-ts/core'
 import { agentDependencies } from '@credo-ts/node'
-import { AskarModule, AskarWallet } from '@credo-ts/askar'
+import { AskarModule } from '@credo-ts/askar'
 import { ariesAskar } from '@hyperledger/aries-askar-nodejs'
-import jose, { importJWK, exportJWK, JWK, generateKeyPair } from "jose";
+import { JWK } from "jose";
+import { createDIDPrivateKey, createDIDPublicJWK } from "./dids/create";
+import { resolveDID, getAllDIDsinWallet } from "./dids/resolve";
 
 const privKey: JWK = {
-	"crv": "Ed25519",
-	"d": "WMyT0IX3Zoh8L_fNjh5RznAreHUZfE3cLBRTUYc9jIk",
-	"x": "76mKj3Ajcb0FAR2X5vJ-Uwj1J1mmD4N0xe5ErV2HCY8",
-	"kty": "OKP"
+    "crv": "Ed25519",
+    "d": "hLraQTB2lPhdbSzT8Am3L2Q24UtHkZzEuolUoE92yoU",
+    "x": "EvlIMqVP4gltfy3bxwK-3UUFzlL-NTfDBf5nvhDKcwQ",
+    "kty": "OKP"
 };
 
+const pubKey: JWK = {
+    "crv": "Ed25519",
+    "x": "EvlIMqVP4gltfy3bxwK-3UUFzlL-NTfDBf5nvhDKcwQ",
+    "kty": "OKP",
+    "kid": "0DaI_ptRiWdPmEmhTD2c_DhkM8bngE9DMKONm62lSJ0"
+};
 
-const importkeys = async (priv: JWK) => {
-	const privKeyUint = await importJWK(priv, 'EdDSA');
-	console.log(TypedArrayEncoder.fromString(JSON.stringify(priv)));
-
-}
-const createWalletFromPrivKey = async (priv: JWK) => {
-	// const privKeyUint = await importJWK(priv, 'EdDSA');
-
+const setupAgent = async () => {
+	// Credo Initialization configuration
+	// setup store and wallet
 	const config: InitConfig = {
 		label: 'docs-agent-nodejs',
 		walletConfig: {
-			id: 'wallet-id',
+			id: 'wallet-id-2',
 			key: 'testkey0000000000000000000000000',
 			storage: {
 				type: 'sqlite',
-				config: {
-					inMemory: true,
-				}
+				// config: {
+				// 	inMemory: true,
+				// }
 			},
 		},
 	}
 	
+	// create agent - here Aries Askar
 	const agent = new Agent({
 		config,
 		dependencies: agentDependencies,
@@ -45,26 +49,30 @@ const createWalletFromPrivKey = async (priv: JWK) => {
 			}),
 		},
 	});
-	await agent.initialize();
-	const walletConfig: WalletCreateKeyOptions = {
-		keyType: KeyType.Ed25519,
-		privateKey: TypedArrayEncoder.fromString('96213c3d7fc8d4d6754c7a0fd969598m'),
-	};
 
-	const myKey = await agent.wallet.createKey(walletConfig);
-	const  didcreate: KeyDidCreateOptions = {
-		method: 'key',
-		options: {
-			key: myKey,
-		},
-	};
-	const did = await agent.dids.create(didcreate);
-	console.log(did);
-	console.log('vfm:', JSON.stringify(did.didState.didDocument?.verificationMethod));
+	// operations in the credo wallet can only be performed once the agent is initialized
+	await agent.initialize();
+
+	return agent;
+}
+const main = async () => {
+
+	const agent = await setupAgent();
+
+	const did1 = await createDIDPublicJWK(agent, pubKey);
+	console.log("DID1 ::: ",  did1.didState.did)
+	// this method creates a key in the wallet and then generates the DID
+	// this will throw error if we run the function again with the same private key
+	// recommend to change the private key or else comment this code.
+	const did2 = await createDIDPrivateKey(agent, "96213c3d7fc8d4d6754c7a0fd969598l");
+	console.log("DID2 ::: ",  did2.didState.did);
+
+	const didDoc = await resolveDID(agent, did1.didState.did!);
+
+	const allDIDs = await getAllDIDsinWallet(agent, "key");
 
 	await agent.wallet.close();
 	await agent.shutdown();
 } 
 
-// createWalletFromPrivKey(privKey);
-importkeys(privKey);
+main();
